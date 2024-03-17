3ee0ba7db0e35bd9ca944ce1b37b9f337eb0aca9
Irvandzawinnuha <72366333+Irvandzawinnuha@users.noreply.github.com>
2024-03-17 23:02:49 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("1302223076");
+        data.PrintData();
+
+        Console.WriteLine("Halo user Praktikan, Irvan Dzawin Nuha kelas SE4602, S-1 Rekayasa Perangkat Lunak, Angkatan 2022, Mata Kuliah Konstruksi Perangkat Lunak");
+    }
+}
